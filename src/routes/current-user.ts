8ab3ecefae07ec19263eb318d70020f5b399e3fe
fancier <fancier@proton.me>
2023-01-30 13:49:34 +0400

    current user

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,8 +1,19 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+
 const router = express.Router()
 
-router.get('/api/users/currentuser', (_req, res) => {
-  res.send('CurrentUser')
+router.get('/api/users/currentuser', async (req, res) => {
+  if (!req.session?.jwt) {
+    return res.json({ currentUser: null })
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, 'abc')
+    return res.json({ currentUser: payload })
+  } catch (error) {
+    return res.json({ currentUser: null })
+  }
 })
 
 export { router as currentUserRouter }
