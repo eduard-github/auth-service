cbc748e016a7573a6b4501baa822dd000e0e38ee
fancier <fancier@proton.me>
2023-01-30 15:35:10 +0400

    current user, auth require middleware

---
+++ src/middlewares/current-user.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+
+interface UserPayload {
+  id: string
+  email: string
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload
+    }
+  }
+}
+
+export function currentUser(req: Request, _res: Response, next: NextFunction) {
+  if (!req.session?.jwt) {
+    next()
+  }
+
+  try {
+    const payload = jwt.verify(req.session?.jwt, 'abc') as UserPayload
+    req.currentUser = payload
+  } catch (error) { }
+
+  next()
+}
--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,19 +1,22 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
+import { currentUser } from '../middlewares/current-user'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', async (req, res) => {
-  if (!req.session?.jwt) {
-    return res.json({ currentUser: null })
-  }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  return res.json({ currentUser: req.currentUser || null })
 
-  try {
-    const payload = jwt.verify(req.session.jwt, 'abc')
-    return res.json({ currentUser: payload })
-  } catch (error) {
-    return res.json({ currentUser: null })
-  }
+  // if (!req.session?.jwt) {
+  //   return res.json({ currentUser: null })
+  // }
+  //
+  // try {
+  //   const payload = jwt.verify(req.session.jwt, 'abc')
+  //   return res.json({ currentUser: payload })
+  // } catch (error) {
+  //   return res.json({ currentUser: null })
+  // }
+
 })
 
 export { router as currentUserRouter }
