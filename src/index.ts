3b9343c300fd273a49c7b240be344eb4a4716a5b
fancier <fancier@proton.me>
2023-02-01 19:29:29 +0400

    env

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import mongoose from 'mongoose'
 import { app } from './app';
 
+const PORT = process.env.API_PORT || 5000;
+const MONGO_INITDB_ROOT_USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME || 'root'
+const MONGO_INITDB_ROOT_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD || '123456'
+
 const start = async () => {
-  // if (!process.env.JWT_KEY) {
-  //   throw new Error('JWT must be defined')
-  // }
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT must be defined')
+  }
   try {
     mongoose.set('strictQuery', false)
-    await mongoose.connect("mongodb://root:123456@mongo:27017");
-    console.log('Connected to MongoDB ---- ')
+    await mongoose.connect(`mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@mongo:27017`);
   } catch (error) {
     console.log('ERR ---- ', error)
   }
-  app.listen(5000, () => {
-    console.log('Server listening on port 5000')
+  app.listen(PORT, () => {
+    console.log(`API service listening on port ${PORT}`)
   })
 }
 
